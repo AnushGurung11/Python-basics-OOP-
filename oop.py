4fbf42308b66183bd7fe0a88fd769b80f6f7fdb6
AnushGurung11 <anushgrg15@gmai.com>
2025-06-27 12:03:37 +0545

    commenting

--- oop.py
+++ oop.py
@@ -3,9 +3,12 @@
 # Creating a class named dog 
 
 class Dog: 
+
+    # This is considered as the constructor 
     def __init__(self):
         pass
 
+    # Here self represents that this methods is only for the instance of the object
     def bark(self): 
         print("Bark")
 
