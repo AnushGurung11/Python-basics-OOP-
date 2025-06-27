9e03f8421326ec1428c692719fee8b105622c4a6
AnushGurung11 <anushgrg15@gmai.com>
2025-06-27 11:59:35 +0545

    Basic of creating a class and Object

---
+++ oop.py
@@ -0,0 +1,15 @@
+# For the basics of OOP in Python 
+
+# Creating a class named dog 
+
+class Dog: 
+    def __init__(self):
+        pass
+
+    def bark(self): 
+        print("Bark")
+
+# Creating an instance of class Dog 
+
+d = Dog()
+d.bark()
